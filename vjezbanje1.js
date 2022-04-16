4846c37e9a4f30ef333b5abda151a9b33d622059
Jasmin <94010495+JaskoM95@users.noreply.github.com>
2022-04-16 19:23:55 +0200

    JaskoM95/meni

---
+++ vjezbanje1.js
@@ -0,0 +1,15 @@
+var naslovi = $('.naslovi');
+var podnaslovi = $ ('.podnaslovi');
+
+naslovi.eq(0).parent().find('.podnaslovi').show();
+
+naslovi.on ('click', function () {
+
+    podnaslovi.slideUp();
+    $(this).parent().find ('.podnaslovi').slideDown(800);
+
+
+
+
+
+})
